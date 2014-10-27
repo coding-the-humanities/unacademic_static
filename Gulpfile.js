13723bcf892f8bc7e4faa7cf40033b0638102496
Zaturrby <rjkorteschiel@gmail.com>
2014-10-27 17:37:09 +0100

    add gulp to json, and linted yaml

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,22 @@
 var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
-var html = require('./000_HTML.json');
 var watch = require('gulp-watch');
+var yaml = require('gulp-yaml');
 
+var data = 'test.yaml'
 var template = 'level000.hbs';
 
 gulp.task('watch', function () {
-  gulp.watch(template, ['template']);
+  gulp.watch(template, ['yaml2json']);
 });
 
-gulp.task('template', function () {
-  var options = {}
-  
-  return gulp.src('level000.hbs')
-  .pipe(handlebars(html, options))
-  .pipe(rename('000_HTML.html'))
-  .pipe(gulp.dest('dist'));
-});
+gulp.task('to_json', function(){
+	var options = {};
+	gulp.src("yaml/**.*")
+  .pipe(yaml())  
+  .pipe(gulp.dest('json'))
+})
+
+gulp.task('default', ['to_json'])
+
