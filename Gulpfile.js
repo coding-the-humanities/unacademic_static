e3a4428fdfb14b6029cdf010a3ce7320bc6dad7c
TaekeKeuning <taekekeuning@gmail.com>
2014-10-27 16:02:01 +0100

    update

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,11 +2,18 @@ var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
 var html = require('./000_HTML.json');
+var watch = require('gulp-watch');
 
-gulp.task('default', function () {
+var template = 'level000.hbs';
+
+gulp.task('watch', function () {
+  gulp.watch(template, ['template']);
+});
+
+gulp.task('template', function () {
   var options = {}
   
-  return gulp.src('000_HTML.hbs')
+  return gulp.src('level000.hbs')
   .pipe(handlebars(html, options))
   .pipe(rename('000_HTML.html'))
   .pipe(gulp.dest('dist'));
