5504655d5c1ff57684a1c66b508b3ba554413abd
Jan Hein Hoogstad <yeehaa@codingthehumanities.com>
2014-10-28 22:51:52 +0100

    fix fr*@()add -A* gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,7 +9,25 @@ var tap = require('gulp-tap');
 var marked = require('marked');
 var flatten = require('gulp-flatten');
 var deploy = require('gulp-gh-pages');
-var clean = require('gulp-clean');
+var del = require('del');
+var runSequence = require('run-sequence')
+
+// HELPERS
+
+Handlebars.registerHelper('markdown', function(text) {
+  return marked(text);
+});
+
+
+// PATHS
+
+var assets =  [
+  './fonts/**/*',
+  './img/**/*',
+  './js/**/*',
+  './css/**/*',
+  'index.html'
+]
 
 var objectives = 'yaml/objectives/**/*.yaml';
 var levelTemplate = 'level000.hbs';
@@ -17,38 +35,49 @@ var levelTemplate = 'level000.hbs';
 var syllabus = 'yaml/syllabus/**/*.yaml';
 var syllabusTemplate = 'syllabus.hbs';
 
-Handlebars.registerHelper('markdown', function(text) {
-  return marked(text);
+
+// External Tasks
+
+gulp.task('default', ['build', 'watch']);
+
+gulp.task('build', function(cb){
+ runSequence('clean', ['copy', 'compile', 'syllabus'], cb);
 });
 
-gulp.task('default', ['clean', 'build', 'watch']);
-gulp.task('deploy', ['clean', 'build', 'github']);
-gulp.task('build', ['copy', 'compile', 'syllabus']);
+gulp.task('deploy', function(cb){
+ runSequence('build', ['github'], cb);
+});
+
+
+// Watches
 
 gulp.task('watch', function () {
   gulp.watch([levelTemplate, objectives], ['compile'])
   gulp.watch([syllabus, syllabusTemplate], ['syllabus'])
+  gulp.watch([assets], ['copy'])
 });
 
-gulp.task('clean', function () {
-  return gulp.src('./dist', {read: false})
-    .pipe(clean());
+
+// Internal Tasks
+
+gulp.task('clean', function (cb) {
+  return del('dist', cb)
 });
 
 gulp.task('copy', function () {
-  var options = {};
-  return gulp.src(  ['./fonts/**/*',
-                    './img/**/*',
-                    './js/**/*',
-                    './css/**/*',
-                    'index.html'], { "base" : "." } 
-                  )
+  return gulp.src(assets, { "base" : "." })
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('clean', function (cb) {
+  del('dist', cb)
+});
+
+// These two tasks need to be refactored after we start using partials
+
 gulp.task('compile', function(){
   var options = {};
-  gulp.src(objectives)
+  return gulp.src(objectives)
     .pipe(yaml())
     .pipe(tap(function(file, t){
       var hbs = fs.readFileSync(levelTemplate);
@@ -66,12 +95,11 @@ gulp.task('compile', function(){
 
 gulp.task('syllabus', function(){
   var options = {};
-  gulp.src(syllabus)
+  return gulp.src(syllabus)
     .pipe(yaml())
     .pipe(tap(function(file, t){
       var hbs = fs.readFileSync(syllabusTemplate);
       var template = Handlebars.compile(hbs.toString());
-
       var json = JSON.parse(file.contents.toString());
       var html = template(json);
 
@@ -84,6 +112,6 @@ gulp.task('syllabus', function(){
 
 gulp.task('github', function () {
   var options = {};
-  return gulp.src('dist/**/*')
+  gulp.src('dist/**/*')
     .pipe(deploy(options));
 });
